70608449bd5d1f918944852b7deca8fdc8f49bbf
ODoctorDu <odoctordu@gmail.com>
2021-03-05 16:45:17 -0300

    Sum Calculator

---
+++ Ex3PONTO1EstruturaSequencial/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3PONTO1EstruturaSequencial
+{
+    class Calculadora
+    {
+        //--------------------[ATRIBUTOS]
+       
+        public double Total;       //Atributos sempre com 1ªLetra Maiúscula
+
+        //--------------------[MÉTODOS]
+        public void Soma (double valor1, double valor2)
+        {
+            Total = valor1 + valor2;
+        }
+    }
+}
---
+++ Ex3PONTO1EstruturaSequencial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3PONTO1EstruturaSequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora operacao1 = new Calculadora();
+
+            Console.WriteLine("Faça um programa para ler dois valores inteiros, e depois mostrar na tela  ");
+            Console.WriteLine("a soma desses números com uma mensagem explicativa, conforme exemplos.\n");
+
+            
+            Console.Write("Digite o primeiro valor para somar: ");
+            double leitura1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor para somar: ");
+            double leitura2 = double.Parse(Console.ReadLine());
+
+            operacao1.Soma(leitura1, leitura2);
+
+            Console.WriteLine("\nO Resultado sa soma é: " + operacao1.Total);
+
+            Console.ReadKey();
+        }
+    }
+}
